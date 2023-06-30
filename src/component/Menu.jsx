af15d9b8de2cbc6c930dcd401995a358d3d5bf3f
Raj <39922631+raj-28@users.noreply.github.com>
2023-06-30 06:40:46 +0000

    new Home added

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Menu = () => {
   return <div className="px-2 md:px-0 py-3 space-y-2 md:space-y-0 md:space-x-2 font-medium text-slate-700">
     <Link
-      to="Home"
+      to="/"
       className="block md:inline-block px-3 py-2 rounded-md text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
     >
       Home
