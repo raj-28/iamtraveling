497eaaf77ea8f7fc2107a0eb11faf07766a55ea9
Raj <39922631+raj-28@users.noreply.github.com>
2023-07-06 12:05:51 +0000

    vercel added

--- client/src/component/TravelDetails.jsx
+++ client/src/component/TravelDetails.jsx
@@ -37,7 +37,7 @@ const TravelForm = () => {
     } else {
       try {
         // Send the form data to the backend
-        const response = await fetch("http://localhost:5000/api/submissions", {
+        const response = await fetch("https://iamtraveling.vercel.app/api/submissions", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: 'https://triptravel.vercel.app'}));
 
 
 // Connect to the database
